131b4524b4feb4339ddd0ebb2dc08d3c00738bff
TBG <46306028+TBG1000@users.noreply.github.com>
2023-05-10 23:05:09 -0600

    Comenzar con funcionamiento de login/register

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./app/models/User");
 const indexRoutes = require("./app/routes/indexRoutes");
 const adminRoutes = require("./app/routes/adminRoutes");
+const userRoutes = require("./app/routes/userRoutes");
 
 mongoose
     .connect("mongodb+srv://CinemaSurfAdmin:giq1pqvGDDFrDaek@cinemasurfdb.yqvttyu.mongodb.net/movies", { useNewUrlParser: true, useUnifiedTopology: true })
@@ -11,7 +16,8 @@ mongoose
         app.use(express.json());
         app.use("/index", indexRoutes);
         app.use("/admin", adminRoutes);
-        
+        app.use("/user", userRoutes);
+
         app.get("/", (req, res) => {
             res.sendFile(__dirname + "/app/views/pages/index.html")
         });
@@ -23,6 +29,19 @@ mongoose
         app.get("/admin", (req, res) => {
             res.sendFile(__dirname + "/app/views/pages/admin.html")
         });
+
+        app.use(session({
+            secret: "secret",
+            resave: false,
+            saveUninitialized: false
+        }));
+
+        app.use(passport.initialize());
+        app.use(passport.session());
+
+        passport.use(new LocalStrategy(User.authenticate()));
+        passport.serializeUser(User.serializeUser());
+        passport.deserializeUser(User.deserializeUser());
 
         app.listen(3000, () => {
             console.log("Server has started!")
---
+++ app/routes/userRoutes.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const passport = require("passport");
+const User = require("../models/User");
+const router = express.Router();
+
