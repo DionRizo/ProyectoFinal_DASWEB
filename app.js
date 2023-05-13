eabe64c47e29c64cebf078e3c437c24ccaf377ae
Dion Rizo Velarde <dionrvelarde@hotmail.com>
2023-05-12 18:04:44 -0600

    instalacion de paquetes nuevos

--- app.js
+++ app.js
@@ -1,54 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://CinemaSurfAdmin:giq1pqvGDDFrDaek@cinemasurfdb.yqvttyu.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
-
-// Definir el esquema para el modelo de usuarios
-const usuarioSchema = new mongoose.Schema({
-    email: String,
-    password: String
-});
-
-  // Crear el modelo de usuarios
-const Usuario = mongoose.model('Usuario', usuarioSchema);
-
-
-
-// Configurar el middleware para manejar los datos del formulario
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Manejar la petición POST para el registro de usuarios
-app.post('/registro', function(req, res) {
-  // Crear un nuevo usuario utilizando los datos del formulario
-  const nuevoUsuario = new Usuario({
-    email: req.body.email,
-    password: req.body.password
-  });
-
-  // Guardar el nuevo usuario en la base de datos
-  nuevoUsuario.save(function(err) {
-    if (err) {
-      // Manejar los errores de la base de datos
-      console.log(err);
-      res.send('Error al registrar el usuario');
-    } else {
-      // Redirigir al usuario a la página de inicio de sesión
-      res.redirect('/login');
-    }
-  });
-});
-
-
-const app = express();
-const fs = require('fs');
-const path = require('path');
-
-
-
-
-const PORT = 3000;
-=======
 const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
@@ -68,7 +17,6 @@ mongoose
         app.use("/index", indexRoutes);
         app.use("/admin", adminRoutes);
         app.use("/stores", storesRoutes);
->>>>>>> final
 
         app.get("/", (req, res) => {
             res.sendFile(__dirname + "/app/views/pages/index.html")
